46c8c0fe17a820bafbf3eb12780869311a61f134
barokah001 <alimibarakat001@gmail.com>
2025-05-09 11:55:21 +0100

    added a JavaScript file

---
+++ script.js
@@ -0,0 +1,44 @@
+const sweetFruits = [
+  "Banana",
+  "Orange",
+  "Watermelon",
+  "Pear",
+  "Apple",
+  "Mango",
+  "Dates",
+  "Guava",
+  "Pineapple",
+  "Papaya (ripe pawpaw)",
+  "Soursop",
+  "Tangerine",
+  "Grapes (imported)",
+];
+const sourFruits = [
+  "Lemon",
+  "Strawberry",
+  "Lime",
+  "Lemon",
+  "Tamarind",
+  "Orange (some varieties)",
+  "Unripe mango",
+  "Ube (African pear – slightly sour when unripe)",
+  "Baobab fruit",
+  "Star apple (Agbalumo – when unripe or slightly fermented)",
+];
+const bitterFruits = [
+  "Bitter kola",
+  "Bitter orange (Seville orange)",
+  "Garden egg (some varieties)",
+  "Unripe pawpaw (papaya)",
+  "African walnut (when boiled, slightly bitter)",
+  "Baobab fruit (tangy-bitter taste)",
+];
+const tastelessFruits = [
+  "Cucumber",
+  "Garden egg (white variety)",
+  "Unripe watermelon",
+  "Coconut water (from mature coconut)",
+  "Cashew apple (can be watery if not fully ripe)",
+  "Tomato (considered fruit, very mild flavor)",
+];
+
